8b0ec52e33147d3fbee661a00678f9498e8820df
Chenzi Cao <chcao@suse.com>
2024-08-13 09:38:55 +0200

    Check agama profiles for default installation

--- tests/yam/agama/agama.pm
+++ tests/yam/agama/agama.pm
@@ -18,9 +18,12 @@ use testapi qw(
 
 sub run {
     my $self = shift;
-    my $test = get_required_var('AGAMA_TEST');
+    #my $test = get_required_var('AGAMA_TEST');
 
-    assert_script_run("playwright test --trace on --project chromium --config /usr/share/e2e-agama-playwright tests/" . $test . ".spec.ts", timeout => 1200);
+    assert_screen('select_product', 120);
+    select_console('root-console');
+    assert_script_run("./agama-integration-tests tests/test_root_password.js", timeout => 1200);
+    #assert_script_run("playwright test --trace on --project chromium --config /usr/share/e2e-agama-playwright tests/" . $test . ".spec.ts", timeout => 1200);
     $self->upload_traces();
     enter_cmd 'reboot';
 }
