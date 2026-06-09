e275c335d58e7e7b8bbd39423d02ec032bdcc5a9
Chenzi Cao <chcao@suse.com>
2026-06-09 09:33:02 +0200

    add testmodule to register addons only

---
+++ tests/yam/migration/register_addons.pm
@@ -0,0 +1,17 @@
+# Copyright 2026 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Register the original system before migration
+# Maintainer: QE Installation and Migration (QE Iam) <none@suse.de>
+
+use Mojo::Base 'y2_module_consoletest';
+use testapi;
+use registration;
+
+sub run {
+    select_console 'root-console';
+
+    register_addons_cmd();
+}
+
+1;
